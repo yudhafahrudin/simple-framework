0bc95212bb5f84de76f49905cc27e54d901a42bf
yudafahrudin <ahmadyudafahrudin@gmail.com>
2018-01-25 16:20:40 +0700

    add error html

---
+++ app/views/error/404.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<html>
+    <h1>Halaman Yang Anda Tuju Tidak ditemukan</h1>
+</html>
